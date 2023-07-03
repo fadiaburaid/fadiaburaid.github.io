7b8a4b1482e5ee0ebbec03fb0e246de172cab24d
Fadi Fadi Abu Raid <fadiaburaid@gmail.com>
2023-07-03 16:12:04 +0800

    await

--- jsm/worker.js
+++ jsm/worker.js
@@ -1,12 +1,12 @@
-
+pipeline = require('https://cdn.jsdelivr.net/npm/@xenova/transformers@2.3.0');
 const file = "tos.pcm";
 const model = "Xenova/whisper-base.en";
 
 
-const pipe = await pipeline("automatic-speech-recognition", model);
 var content="";
 
 onmessage = async function(e) {
+	const pipe = await pipeline("automatic-speech-recognition", model);
 	let result = await pipe(e.data, {
 			chunk_length_s: 30,
 			stride_length_s: 5,
